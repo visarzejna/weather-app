aa14ceaabac9b1c9dcaf967426fcdfc949d3308c
visarzejna <visarzejna@gmail.com>
2022-11-10 01:57:46 +0100

    First commit

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://weatherapi-com.p.rapidapi.com';
+
+const options = {
+  params: {},
+  headers: {
+    'X-RapidAPI-Key': '078b0be2f3msh0b44c50748119a5p1a4672jsnfbe1b8a8ab5a',
+    'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+  }
+};
+
+export const fetchFromAPI = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+    return data;
+}
+
+export const getData = async () => {
+    const res = await axios.get('https://geolocation-db.com/json/')
+
+    return res; 
+  }
---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'weather-background': "url('https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')",
+      }
+    }
+  },
+  plugins: [],
+}
